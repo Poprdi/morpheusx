57b6dc82d439f5d60025e676cc5af6bafe23de94
T. Andrew Davis <0x5F_pop_rdi@proton.me>
2026-02-21 16:57:27 +0100

    Remove unused module declarations from TUI

--- bootloader/src/tui/mod.rs
+++ bootloader/src/tui/mod.rs
@@ -1,13 +1,5 @@
-pub mod boot_sequence;
 pub mod debug;
-pub mod distro_downloader;
-pub mod distro_launcher;
 pub mod input;
-pub mod installer_menu;
-pub mod iso_manager;
-pub mod logo;
 pub mod main_menu;
 pub mod rain;
 pub mod renderer;
-pub mod storage_manager;
-pub mod widgets;
